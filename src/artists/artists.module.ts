9d1ffdf22059047cf8cac2fe0ab00eeefb2ed677
Cédric BOISSINOT <cedric.boissinot99@gmail.com>
2021-05-10 17:16:50 +0200

    correct module artist

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArtistRepository } from './artist.repository';
 import { ArtistsController } from './artists.controller';
 import { ArtistsService } from './artists.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ArtistRepository])],
   controllers: [ArtistsController],
   providers: [ArtistsService]
 })
